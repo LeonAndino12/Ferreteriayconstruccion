96dcdcc907432951b9b9f5abf70c49bd2d1e8d5c
LeonAndino12 <leonandino22@gmail.com>
2025-06-21 16:11:28 -0300

    21/6 script.js

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
         const products = [
-            { name: 'POLAR COLOR', category: 'Tanque de agua', image: 'https://www.sanyojafep.com/imgs/polar-latex-color.jpg' },
-            { name: 'Pintura 1', category: 'Pintura', image: 'https://media.xcons.com.ar/media/catalog/product/q/u/quimica_llana-lt1180-1l.jpg', link: 'product-detail2.html' },
-            { name: 'Membrana 1', category: 'Membrana', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTu7SCIcIep2vXzTVrdy5iMC0H6TSHgt5W0iw&s' },
+
+            { name: 'MEMBRANA EN PASTA <br><br> $55.000', category: 'Membrana', image: 'https://acdn-us.mitiendanube.com/stores/003/392/001/products/recuplast-tradicional-para-techos-membrana-en-pasta-20-lt-70c41f69fb83a8c07c17368719293618-640-0.webp' },,
+            { name: 'VERNIER SUPERCAPA <br><br> $55.000', category: 'Membrana', image: 'vernier-supercapa.jpg' },
+
+            { name: 'MASILLA MAS ENDUIDO <br><br> $55.000', category: 'Masillas', image: 'https://www.polacrin.com.ar/img/categorias/polacrin_masilla_mas_enduido.jpg' },
+            { name: 'MASILLA FORMULA POLIESTER <br><br> $55.000', category: 'Masillas', image: 'https://zeocarweb.zeocar.com/imgs/38-FormulaPoliester.webp' },
+            { name: 'ENDUIDO PLASTICO <br><br> $55.000', category: 'Masillas', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTj4O6H9y64HpOB3Re0xrdMGmOCetmn0wkN_v98rvo-TzDt4os61sTU4t8ZaB2q8WnFqIg&usqp=CAU' },
+
             { name: 'Herramienta 1', category: 'Herramientas', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0pZ68_ijMqTYEun9EXgF-tL_nkba1k1wnyA&s' },
             { name: 'Tanque 2', category: 'Tanque de agua', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXh3YIiFxdGn-6EfgsSYJwf6vtPYQdZOxNrA&s' },
             { name: 'Herramienta 1', category: 'Herramientas', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0pZ68_ijMqTYEun9EXgF-tL_nkba1k1wnyA&s' },
@@ -11,11 +16,30 @@
             { name: 'Herramienta 1', category: 'Herramientas', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0pZ68_ijMqTYEun9EXgF-tL_nkba1k1wnyA&s' },
             { name: 'Tanque 2', category: 'Tanque de agua', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXh3YIiFxdGn-6EfgsSYJwf6vtPYQdZOxNrA&s' },
             { name: 'Herramienta 1', category: 'Herramientas', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0pZ68_ijMqTYEun9EXgF-tL_nkba1k1wnyA&s' },
-            { name: 'Tanque 2', category: 'Tanque de agua', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXh3YIiFxdGn-6EfgsSYJwf6vtPYQdZOxNrA&s'  },            
+            { name: 'Tanque 2', category: 'Tanque de agua', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXh3YIiFxdGn-6EfgsSYJwf6vtPYQdZOxNrA&s' },            
             { name: 'Herramienta 1', category: 'Herramientas', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0pZ68_ijMqTYEun9EXgF-tL_nkba1k1wnyA&s' },
             { name: 'Tanque 2', category: 'Tanque de agua', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXh3YIiFxdGn-6EfgsSYJwf6vtPYQdZOxNrA&s' },
             { name: 'Tanque 2', category: 'Tanque de agua', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXh3YIiFxdGn-6EfgsSYJwf6vtPYQdZOxNrA&s' },
-            { name: 'Pintura 2', category: 'Pintura', image: 'https://www.sherwin.com.ar/wp-content/uploads/2021/11/REP-LPP.jpg'  },
+
+            { name: 'ESMALTE SINTETICO <br><br> $55.000', category: 'Pintura', image: 'https://tersuave.com.ar/wp-content/uploads/sites/2/2024/11/TERSILUX-ESMALTE-SINTETICO-3en1-SATINADO.png' },
+            { name: 'LATEX INTERIOR-EXTERIOR <br><br> $55.000', category: 'Pintura', image: 'latex-riobra.jpg' },
+            { name: 'POLAR COLOR <br><br> $55.000', category: 'Pintura', image: 'https://www.sanyojafep.com/imgs/polar-latex-color.jpg' },
+            { name: 'POLAR INTERIOR MATE <br><br> $55.000', category: 'Pintura', image: 'https://www.sanyojafep.com/imgs/polar-interior-nuevo.png'},
+            { name: 'POLAR SATINADO BLANCO <br><br> $55.000', category: 'Pintura', image: 'https://www.sanyojafep.com/imgs/polar-satinado-nuevo.png'},
+            { name: 'POLAR INTERIOR-EXTERIOR <br><br> $55.000', category: 'Pintura', image: 'https://www.sanyojafep.com/imgs/polar-interior-exterior-nuevo.png'},
+            { name: 'POLAR CIELO RAZOS <br><br> $55.000', category: 'Pintura', image: 'https://www.sanyojafep.com/imgs/polar-cielorasos-nuevo.png'},
+            { name: 'POLAR FRENTES <br><br> $55.000', category: 'Pintura', image: 'https://www.sanyojafep.com/imgs/polar-exterior-nuevo.png'},
+            { name: 'POLAR FLEX MUROS Y FRENTES <br><br> $55.000', category: 'Pintura', image: 'https://www.sanyojafep.com/imgs/polar-flex-muros-y-frentes.jpg'},
+            { name: 'POLAR FLEX MUROS Y FRENTES POLIURETANICO <br><br> $55.000', category: 'Pintura', image: 'https://sanyojafep.com/imgs/polar-flex-muros-y-frentes-poliuretanico.jpg'},
+            { name: 'POLAR FLEX MEMBRANA LIQUIDA <br><br> $55.000', category: 'Pintura', image: 'https://sanyojafep.com/imgs/polar-flex-membrana.jpg'},
+            { name: 'POLAR FLEX MEMBRANA POLIURETANICA <br><br> $55.000', category: 'Pintura', image: 'https://www.sanyojafep.com/imgs/polar-flex-membrana-poliuretanica.jpg'},
+            { name: 'POLAR FLEX MEMBRANA FIBRADA <br><br> $55.000', category: 'Pintura', image: 'https://www.sanyojafep.com/imgs/polar-flex-membrana-con-fibra.png'},
+
+            { name: 'AEROSOLES COLOR <br><br> $55.000', category: 'Pintura', image: 'https://www.dantepinturerias.com/wp-content/uploads/2020/02/aerosol-toke-1.jpg'},
+
+            { name: 'FIJADOR PARA PIEDRAS <br><br> $55.000', category: 'Exteriores', image: 'https://sanyojafep.com/imgs/fijador-piedras.png'},
+
+
         ];
 
         function renderProducts(filter) {
